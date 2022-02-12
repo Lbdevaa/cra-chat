c6b9d5026f076d4818b89b1e4f6f0fc246ccad73
Meshcheryakov Andrey <mesheryakov97@gmail.com>
2022-02-13 02:46:57 +0500

    $fix: clean-up

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import Welcome from './Welcome';
-import Clock from './Clock';
-import List from './List';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          <hr />
-          <Welcome >
-            Bob
-          </Welcome >
-
-          <List></List>
-
-          <hr />
-          <Clock />
-        </a>
-      </header>
+      <h1>
+        Chat
+      </h1>
     </div>
   );
 }
--- src/Welcome.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Welcome({ children }) {
-  return <h1>Sup, {children} </h1>;
-}
-
-export default Welcome;
